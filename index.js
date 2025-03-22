2687880ed589c808aa670f7f51f86334ae7faf89
Santhosh Penupotula <santhoshkumar.penupotula@gmail.com>
2025-03-22 11:01:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get('/users', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
